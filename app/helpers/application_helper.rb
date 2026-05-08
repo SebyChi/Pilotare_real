429269c6010621b7ec16ad2d9da64a79a6b9b4a0
Seby <seby@MacBook-Pro-Seby.local>
2026-05-08 20:36:15 +0300

    Made navi dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  def return_to_main
+    link = link_to "Main", main_path
+    link.html_safe
+  end
+
+  def physics_page?
+    if request.path.include?("/physics")
+      return_to_main
+    else
+      link = link_to "Physics", physics_path()
+      link.html_safe
+    end
+  end
+
+  def chemistry_page?
+    if request.path.include?("/chemisrty")
+      return_to_main
+    else
+      link = link_to "Chemistry", chemistry_path()
+      link.html_safe
+    end
+  end
+
+  def biology_page?
+    if request.path.include?("/biology")
+      return_to_main
+    else
+      link = link_to "Biology", biology_path()
+      link.html_safe
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get "/physics", to: "navi#physics", as: :physics
   get "/biology", to: "navi#biology", as: :biology
   get "/chemistry", to: "navi#chemistry", as: :chemistry
-  root "application#main"
+  root "application#main", as: :main
 end
