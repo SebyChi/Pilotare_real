b02ac5b91b6780efb0b9dad7d689daa2cc3885f7
Seby <seby@MacBook-Pro-Seby.local>
2026-05-12 18:53:14 +0300

    Added biology and chem controller and routes

---
+++ app/controllers/biology_controller.rb
@@ -0,0 +1,2 @@
+class BiologyController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
+
   get "/physics", to: "navi#physics", as: :physics
   get "/biology", to: "navi#biology", as: :biology
   get "/chemistry", to: "navi#chemistry", as: :chemistry
+
+  get "/sun", to: "chemistry#sun", as: :sun
+  get "/energy_storage", to: "chemistry#energy_storage", as: :energy_storage
+  get "/hydrogen", to: "chemistry#hydrogen", as: :hydrogen
+  get "/bio_fuel", to: "chemistry#bio_fuel", as: :bio_fuel
+
   root "application#main", as: :main
 end
