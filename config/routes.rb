770b991787e79c8399335d3c0a74800600629fa3
Seby <seby@MacBook-Pro-Seby.local>
2026-05-13 11:33:30 +0300

    Finished physics main page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,12 @@ Rails.application.routes.draw do
   get "/hydrogen", to: "chemistry#hydrogen", as: :hydrogen
   get "/bio_fuel", to: "chemistry#bio_fuel", as: :bio_fuel
 
+  get "/hidro_energy", to: "physics#hidro_energy", as: :hidro_energy
+  get "/solar_energy", to: "physics#solar_energy", as: :solar_energy
+  get "/wind_energy", to: "physics#wind_energy", as: :wind_energy
+  get "/nuclear_energy", to: "physics#nuclear_energy", as: :nuclear_energy
+  get "/geo_thermal_energy", to: "physics#geo_thermal_energy", as: :geo_thermal_energy
+  get "/tidal_energy", to: "physics#tidal_energy", as: :tidal_energy
+
   root "application#main", as: :main
 end
