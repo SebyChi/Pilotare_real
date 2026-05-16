082161128171e5c582d40293bce335f6e3f21b15
Seby <seby@MacBook-Pro-Seby.local>
2026-05-16 13:48:46 +0300

    Routes for biology

--- app/controllers/biology_controller.rb
+++ app/controllers/biology_controller.rb
@@ -1,2 +1,5 @@
 class BiologyController < ApplicationController
+  def biomass_sustainability; end
+  def biofuel_sustainability; end
+  def climate_change; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,28 @@ Rails.application.routes.draw do
   get "/biology", to: "navi#biology", as: :biology
   get "/chemistry", to: "navi#chemistry", as: :chemistry
 
-  get "/sun", to: "chemistry#sun", as: :sun
-  get "/energy_storage", to: "chemistry#energy_storage", as: :energy_storage
-  get "/hydrogen", to: "chemistry#hydrogen", as: :hydrogen
-  get "/bio_fuel", to: "chemistry#bio_fuel", as: :bio_fuel
+  controller :chemistry do
+    get "/sun", action: :sun, as: :sun
+    get "/energy_storage", action: :energy_storage, as: :energy_storage
+    get "/hydrogen", action: :hydrogen, as: :hydrogen
+    get "/bio_fuel", action: :bio_fuel, as: :bio_fuel
+  end
 
-  get "/hidro_energy", to: "physics#hidro_energy", as: :hidro_energy
-  get "/solar_energy", to: "physics#solar_energy", as: :solar_energy
-  get "/wind_energy", to: "physics#wind_energy", as: :wind_energy
-  get "/nuclear_energy", to: "physics#nuclear_energy", as: :nuclear_energy
-  get "/geo_thermal_energy", to: "physics#geo_thermal_energy", as: :geo_thermal_energy
-  get "/tidal_energy", to: "physics#tidal_energy", as: :tidal_energy
+  controller :physics do
+    get "/hidro_energy", action: :hidro_energy, as: :hidro_energy
+    get "/solar_energy", action: :solar_energy, as: :solar_energy
+    get "/wind_energy", action: :wind_energy, as: :wind_energy
+    get "/nuclear_energy", action: :nuclear_energy, as: :nuclear_energy
+    get "/geo_thermal_energy", action: :geo_thermal_energy, as: :geo_thermal_energy
+    get "/tidal_energy", action: :tidal_energy, as: :tidal_energy
+  end
+
+  controller :biology do
+    get "/biomass_sustainability", action: :biomass_sustainability, as: :biomass_sustainability
+    get "/biofuel_sustainability", action: :biofuel_sustainability, as: :biofuel_sustainability
+    get "/energy_and_climate-change", action: :climate_change, as: :climate_change
+  end
+
 
   root "application#main", as: :main
 end
